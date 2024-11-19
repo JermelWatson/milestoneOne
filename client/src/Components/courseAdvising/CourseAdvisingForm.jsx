5951c75740f262b4e7639547ad7aeecabb92c54f
JermelWatson <renoldowatson@gmail.com>
2024-11-19 14:06:37 -0500

    css testing

--- client/src/Components/courseAdvising/CourseAdvisingForm.jsx
+++ client/src/Components/courseAdvising/CourseAdvisingForm.jsx
@@ -72,7 +72,7 @@ function CourseAdvisingForm() {
             <h1>Course Advising Form</h1>
             <div>
             <h2>History</h2>
-            <label>
+            <label id="label1">
                 Last Term:
                 <input type="text" 
                 name="lastTerm" 
