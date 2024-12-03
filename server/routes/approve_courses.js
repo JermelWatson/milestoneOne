f8997b5ab27b5a280405ddfdf410b3f28fd73795
JermelWatson <renoldowatson@gmail.com>
2024-12-03 00:22:02 -0500

    fixed request

--- server/routes/approve_courses.js
+++ server/routes/approve_courses.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { connection } from "../database/database.js";
-//import { SendMail } from "../utils/SendMail.js";
+
 const approve_courses = Router();
 
 approve_courses.put("/approve_courses", (req, res)=>{
     connection.execute(
-        "UPDATE `Records` SET `status` = 'Approved' WHERE `student_id` = ?"[req.body.student_id],
+        "UPDATE `Records` SET `status` = 'Approved' WHERE `student_id` = ?",[req.body.student_id],
         function(err, result){
             if(err){
                 res.json(err.message)
